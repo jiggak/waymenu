ba7b63bb265483e7d112b61fbac7fdab8bb4c8eb
Josh Kropf <josh@slashdev.ca>
2024-01-28 01:26:00 -0500

    Add filter to implement search

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,4 @@
-use gtk::prelude::*;
+use gtk::{gio, prelude::*};
 use gtk4_layer_shell::{KeyboardMode, Layer, LayerShell};
 use std::{fs, path::PathBuf, sync::Arc};
 
@@ -81,7 +81,7 @@ impl Application {
         window.set_layer(Layer::Top);
 
         // Add action "close" to `window` taking no parameter
-        let action_close = gtk::gio::ActionEntry::builder("close")
+        let action_close = gio::ActionEntry::builder("close")
             .activate(|window: &gtk::ApplicationWindow, _, _| {
                 window.close();
             })
@@ -89,8 +89,8 @@ impl Application {
         window.add_action_entries([action_close]);
 
         let event_ctrl = gtk::EventControllerKey::new();
-        event_ctrl.connect_key_pressed(|keyval, keycode, x, y| {
-            println!("{keyval} {keycode} {x} {y}");
+        event_ctrl.connect_key_pressed(|_, keyval, keycode, state| {
+            println!("keyval:{keyval} keycode:{keycode} state:{state}");
             gtk::glib::Propagation::Proceed
         });
         // connect_key_pressed
@@ -100,13 +100,13 @@ impl Application {
             Commands::Launcher => load_app_list(),
             Commands::Menu => panic!("Menu not implemented")
         };
-        self.build_ui(&window, &items);
+        self.build_ui(&window, items);
 
         // Present window
         window.present();
     }
 
-    fn build_ui(&self, window: &gtk::ApplicationWindow, items: &impl IsA<gtk::gio::ListModel>) {
+    fn build_ui(&self, window: &gtk::ApplicationWindow, items: impl IsA<gtk::gio::ListModel>) {
         let window_box = gtk::Box::builder()
             .orientation(gtk::Orientation::Vertical)
             .name("window-box")
@@ -116,7 +116,24 @@ impl Application {
             .name("input")
             .build();
 
-        let list = self.build_list_view(items);
+        let list_item_search_expr = gtk::PropertyExpression::new(
+            ListItemObject::static_type(),
+            gtk::Expression::NONE,
+            "label"
+        );
+
+        let filter = gtk::StringFilter::builder()
+            .match_mode(gtk::StringFilterMatchMode::Substring)
+            .ignore_case(true)
+            .expression(list_item_search_expr)
+            .build();
+
+        let filter_model = gtk::FilterListModel::new(Some(items), Some(filter.clone()));
+
+        entry.property_expression("text")
+            .bind(&filter, "search", gtk::Widget::NONE);
+
+        let list = self.build_list_view(filter_model);
 
         let scroll = gtk::ScrolledWindow::builder()
             .name("scroll")
@@ -131,9 +148,22 @@ impl Application {
         window.set_child(Some(&window_box));
     }
 
-    fn build_list_view(&self, items: &impl IsA<gtk::gio::ListModel>) -> gtk::ListView {
+    fn build_list_view(&self, filter_model: gtk::FilterListModel) -> gtk::ListView {
+        let sorter = gtk::CustomSorter::new(|obj1, obj2| {
+            let list_item1 = obj1
+                .downcast_ref::<ListItemObject>()
+                .expect("ListItemObject expected");
+            let list_item2 = obj2
+                .downcast_ref::<ListItemObject>()
+                .expect("ListItemObject expected");
+
+            // sorted alphabetically a..z
+            list_item1.label().cmp(&list_item2.label()).into()
+        });
+        let sort_model = gtk::SortListModel::new(Some(filter_model), Some(sorter));
+
         let model = gtk::SingleSelection::builder()
-            .model(items)
+            .model(&sort_model)
             .build();
 
         let factory = gtk::SignalListItemFactory::new();
@@ -208,8 +238,8 @@ fn load_css_content(css: &str) {
     );
 }
 
-fn load_app_list() -> gtk::gio::ListStore {
-    gtk::gio::AppInfo::all().iter()
+fn load_app_list() -> gio::ListStore {
+    gio::AppInfo::all().iter()
         .filter(|a| a.should_show())
         .map(ListItemObject::from)
         .collect()
